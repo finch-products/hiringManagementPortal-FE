e2a04770c827e48bcabba982a80cc215fbdba4e7
EpshitaNinawe <epshitaninawe@gmail.com>
2025-03-08 11:13:23 +0530

    candidates details linked to a particular demand listed

--- src/app/demand-view/demand-view.component.ts
+++ src/app/demand-view/demand-view.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild  } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DemandComponent } from './demand/demand.component';
 @Component({
   selector: 'app-demand-view',
   templateUrl: './demand-view.component.html',
   styleUrl: './demand-view.component.scss'
 })
 export class DemandViewComponent {
+  @ViewChild(DemandComponent) demandComponent!: DemandComponent;
   selectedPdfUrl!: string 
+  constructor(private route:ActivatedRoute){
+  
+    }
 
   handlePdfSelection(pdfUrl: string) {
     this.selectedPdfUrl = pdfUrl;
 
+}
+onCandidatesLinked(): void {
+  const demandId = this.route.snapshot.paramMap.get('id');
+  this.demandComponent.loadData(demandId);
 }
 }
