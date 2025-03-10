0191cdd8d0c8abe51b4dc767010eba8bf70c9a28
Akhila <akhilak1997@gmail.com>
2025-03-10 20:36:09 +0530

    bug fixes

--- src/app/demand-view/demand/demand.component.ts
+++ src/app/demand-view/demand/demand.component.ts
@@ -32,12 +32,26 @@ export class DemandComponent implements OnInit {
 
 public   loadData(demandId: any) {
     const payload = { dem_id: demandId };
+   
     this.httpService.postCandidateByDemandId(payload).subscribe({
       next: (data) => {
         this.demands = data;
-        this.candidates = data.candidates || [];
-      // this.candidates = data.candidates; 
-      console.log("candidates linked", this.candidates);
+        // const existingCandidateIds = new Set((data.candidates || []).map((c: { cdl_id: string }) => c.cdl_id));
+        // this.candidates = data.candidates || [];
+       this.candidates = data.candidates ? [...data.candidates].reverse() : [];
+      // console.log("candidates linked", this.candidates);
+   
+  //     console.log("existingCandidateIds",existingCandidateIds)
+
+  //     this.candidates = data.candidates
+  // ? [...data.candidates]
+  //     .reverse()
+  //     .map(candidate => ({
+  //       ...candidate, // Keeps all existing properties dynamically
+  //       isNew: !existingCandidateIds.has(candidate.cdl_id)
+  //     }))
+  // : [];
+  console.log("Updated candidates list:", this.candidates);
 
       }
     })
