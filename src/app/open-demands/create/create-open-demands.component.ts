16eacf1f5ed3c6ccf72eff508688297cc32bac07
Vanshika Panjwani <vanshikapanjwani0105@gmail.com>
2025-03-20 19:31:55 +0530

    frontend fixes date issue and other fixes

--- src/app/open-demands/create/create-open-demands.component.ts
+++ src/app/open-demands/create/create-open-demands.component.ts
@@ -361,7 +361,7 @@ export class CreateOpenDemandComponent implements OnInit {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Ensure 2-digit month
     const day = String(date.getDate()).padStart(2, '0'); // Ensure 2-digit day
-    return' ${year}-${month}-${day}';
+    return `${year}-${month}-${day}`;
   }
 
 
