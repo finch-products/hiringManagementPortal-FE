e14cb973e87a82147a48b4b7b0afbe8481dfc16f
Vanshika Panjwani <vanshikapanjwani0105@gmail.com>
2025-04-29 12:02:39 +0530

    beautified list , search and updated paginator

--- src/masters/lob/list/list-lob.component.ts
+++ src/masters/lob/list/list-lob.component.ts
@@ -1,5 +1,4 @@
 import { Component, ViewChild } from '@angular/core';
-import { OpenDemand } from '../../../interfaces/open-demand.interface';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -14,19 +13,31 @@ import { HttpService } from '../../../app/services/http.service';
   styleUrl: './list-lob.component.scss'
 })
 export class ListLOBComponent {
-  
+
   displayedColumns: string[] = ['lob_name', 'lob_description', 'lob_clientpartner', 'lob_deliverymanager', 'lob_insertby', 'lob_updateby'];
   dataSource = new MatTableDataSource<LOB>([]);
+
+  pageSizeOptions = [5, 10, 20];
+  pageSize = 5;
+  currentPage = 1;
+  totalItems = 0;
+  pages: number[] = [];
+  totalPages = 0;
+
+  allLOBs: LOB[] = [];
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private http: HttpClient, private lobService: LobService, private httpService: HttpService) { }
+  constructor(private http: HttpClient, private lobService: LobService, private httpService: HttpService) {}
 
   ngOnInit() {
     this.fetchLOB();
-    this.lobService.lobs$.subscribe(lob => {
-      this.dataSource.data = lob;
+    this.lobService.lobs$.subscribe(lobs => {
+      this.allLOBs = [...lobs];
+      this.totalItems = this.allLOBs.length;
+      this.updatePages();
+      this.updateDisplayedLObs();
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
       this.dataSource.filterPredicate = this.createFilter();
@@ -37,6 +48,9 @@ export class ListLOBComponent {
     this.httpService.getLOBDetails().subscribe({
       next: (data) => {
         this.lobService.setInitialData(data.reverse());
+        this.allLOBs = [...data];
+        this.totalItems = this.allLOBs.length;
+        this.updateDisplayedLObs();
       },
       error: (err) => console.error('Error fetching LOBs', err)
     });
@@ -45,14 +59,13 @@ export class ListLOBComponent {
   createFilter(): (data: LOB, filter: string) => boolean {
     return (data: LOB, filter: string): boolean => {
       const searchTerms = filter.toLowerCase();
-      
       return (
         data.lob_name.toLowerCase().includes(searchTerms) ||
         (data.lob_description?.toLowerCase().includes(searchTerms) ?? false) ||
         (data.lob_clientpartner?.emp_name.toLowerCase().includes(searchTerms) ?? false) ||
         (data.lob_deliverymanager?.emp_name.toLowerCase().includes(searchTerms) ?? false) ||
-        (data.lob_insertby_id?.emp_name.toLowerCase().includes(searchTerms) ?? false) ||  // Changed to lob_insertby_id
-      (data.lob_updateby_id?.emp_name.toLowerCase().includes(searchTerms) ?? false) 
+        (data.lob_insertby_id?.emp_name.toLowerCase().includes(searchTerms) ?? false) ||
+        (data.lob_updateby_id?.emp_name.toLowerCase().includes(searchTerms) ?? false)
       );
     };
   }
@@ -62,4 +75,51 @@ export class ListLOBComponent {
     this.dataSource.filter = filterValue;
   }
 
+  updateDisplayedLObs() {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    const currentLobs = this.allLOBs.slice(startIndex, endIndex);
+    this.dataSource.data = currentLobs;
+  }
+
+  updatePages() {
+    this.totalPages = Math.ceil(this.totalItems / this.pageSize);
+    this.pages = [];
+
+    for (let i = 1; i <= Math.min(3, this.totalPages); i++) {
+      this.pages.push(i);
+    }
+    if (this.totalPages > 3) {
+      this.pages.push(-1); // ellipsis
+      this.pages.push(this.totalPages);
+    }
+  }
+
+  onPageSizeChange() {
+    this.currentPage = 1;  // Reset to first page
+    this.updatePages();
+    this.updateDisplayedLObs();  // Update list
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updateDisplayedLObs(); // Update list
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updateDisplayedLObs(); // Update list
+    }
+  }
+
+  goToPage(page: number) {
+    if (page === -1) {
+      return; // Ignore ellipsis
+    }
+    this.currentPage = page;
+    this.updateDisplayedLObs(); // Update list
+  }
 }
